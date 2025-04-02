0bcb914be36edc30b6dbe3ce033c658f5762b0e7
Aeralyn <Aeralyn123@gmail.com>
2025-04-02 17:58:15 -0600

    Axios install and updating tsx files

--- Develop/client/src/api/authAPI.tsx
+++ Develop/client/src/api/authAPI.tsx
@@ -1,9 +1,15 @@
+import axios from "axios";
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
-}
-
-
+  try {
+    // Make a POST request to the login route
+    const response = await axios.post("/api/auth/login", userInfo);
+    return response.data; // Return the response data which should include the token
+  } catch (error) {
+    console.error("Login failed", error); // Log the error for debugging
+    throw new Error("Failed to login"); // Optionally throw an error to handle it in the calling function
+  }
+};
 
 export { login };
